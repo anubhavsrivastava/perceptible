025de232a851e63f9012303a06e7e92d53b57e03
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-07 22:39:38 +0530

    added gitignore and sample

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('anubhavs');
+/************************************************************************************************
+ *                                                                                              *
+ *                              VARIABLES DECLARATION                                           *
+ *                                                                                              *
+ ************************************************************************************************/
+var adIsViewable = true,
+	viewabilityTime = 0,
+	adElement = document.getElementById('ad');
+
+/**
+ * Logs the viewability values in the console
+ *
+ * @override
+ */
+window.log = function() {
+	console.log('Ads is viewable: ', adIsViewable, '\nViewability time of the ad in sec:', viewabilityTime);
+};
+
+/************************************************************************************************
+ *                                                                                              *
+ *                              YOUR IMPLEMENTATION                                             *
+ *                                                                                              *
+ ************************************************************************************************/
