580841d61e31638e013ee28ca25db664f5a95734
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-20 01:41:48 +0530

    fix<scheduler> : Changed visibility handling

--- src/schedulers/visibilityHelper.js
+++ src/schedulers/visibilityHelper.js
@@ -1,5 +1,5 @@
 // Refer : https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
-// Shouldn't need a polyfill with 'onblur', 'onfocus' since it seems to be supported by major browsers
+// Shouldn't need a polyfill since it seems to be supported by major browsers
 // Refer caniuse for browser support: https://caniuse.com/#search=pagevisibility
 
 // Set the name of the hidden property and the change event for visibility
@@ -16,16 +16,44 @@ if (typeof document.hidden !== 'undefined') {
 	visibilityChange = 'webkitvisibilitychange';
 }
 
-document[hidden];
-
-export const onPageVisible = (fn = () => {}) => {
-	document.addEventListener(visibilityChange, fn, false);
-};
-
-export const isPageVisible = () => {
+const isPageVisible = () => {
 	return !document[hidden];
 };
 
-export const isPageHidden = () => {
-	return document[hidden];
+// const isPageHidden = () => {
+// 	return document[hidden];
+// };
+
+export const onPageVisibilityChange = (fn = () => {}, attentionMode = true) => {
+	let lastVisibility = undefined;
+
+	// When window is switched and switched back - both pageVisibilty API event and 'focus' event are triggered.
+	// The callback handler in each event ensure that callback(fn) will be called only if there is
+	// actual change in visibility by checking previousState(lastVisibility).
+	// The sequence of execution isn't guaranteed but sequential execution is guaranteed in `task` in event loop
+
+	document.addEventListener(visibilityChange, () => {
+		let currentVisibility = isPageVisible();
+		console.log(currentVisibility, lastVisibility);
+		if (lastVisibility != currentVisibility) {
+			fn(currentVisibility);
+			lastVisibility = currentVisibility;
+		}
+	});
+
+	if (attentionMode) {
+		window.addEventListener('blur', () => {
+			if (lastVisibility != false) {
+				fn(false);
+				lastVisibility = false;
+			}
+		});
+
+		window.addEventListener('focus', () => {
+			if (lastVisibility != true) {
+				fn(true);
+				lastVisibility = true;
+			}
+		});
+	}
 };
