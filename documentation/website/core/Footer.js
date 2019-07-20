89b8e7c7300b78dd5e73029f803e6dd986de4fd6
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-21 01:03:08 +0530

    docs<footer> :updated links

--- documentation/website/core/Footer.js
+++ documentation/website/core/Footer.js
@@ -35,15 +35,15 @@ class Footer extends React.Component {
 						<a href={this.docUrl('architecture', this.props.language)}>Advance Guide</a>
 					</div>
 					<div>
-						<h5>Community</h5>
-						<a href={this.pageUrl('users.html', this.props.language)}>User Showcase</a>
-						<a href="https://stackoverflow.com/questions/tagged/" target="_blank" rel="noreferrer noopener">
+						<h5>Social</h5>
+						{/* <a href={this.pageUrl('users.html', this.props.language)}>User Showcase</a> */}
+						{/* <a href="https://stackoverflow.com/questions/tagged/" target="_blank" rel="noreferrer noopener">
 							Stack Overflow
-						</a>
-						<a href="https://discordapp.com/">Project Chat</a>
-						<a href="https://twitter.com/" target="_blank" rel="noreferrer noopener">
+						</a> */}
+						<a href="https://twitter.com/onlyanubhav" target="_blank" rel="noreferrer noopener">
 							Twitter
 						</a>
+						<a href="mailto:anubhav.srivastava00@gmail.com">Email</a>
 					</div>
 					<div>
 						<h5>More</h5>
