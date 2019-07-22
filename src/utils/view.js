a0f94d1852ee43b4aae8837540e1270003ce13c5
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-22 19:11:58 +0530

    chore<docs> : added in-code docs

--- src/utils/view.js
+++ src/utils/view.js
@@ -1,19 +1,37 @@
+/**
+ * Provides current Scroll Position and current resolution of the viewport
+ * @return {object} - {left, top} : Scroll Position from (0,0) of the view, {height, width} : Resolution of current visible Viewport
+ */
 export const getCurrentViewport = function() {
 	if (document.documentElement && document.documentElement.clientHeight) {
 		return { ...getCurrentScrollPosition(), height: document.documentElement.clientHeight, width: document.documentElement.clientWidth };
 	}
-	return { left: 0, right: 0, width: 0, height: 0 };
+	return { left: 0, top: 0, width: 0, height: 0 };
 };
 
+/**
+ * Provides current Scroll Position
+ * @return {object} - {left, top} : Scroll Position from (0,0)
+ */
 export const getCurrentScrollPosition = function() {
 	return { left: window.pageXOffset, top: window.pageYOffset };
 };
 
+/**
+ * Provides Position of current element and resolution
+ * @param {Element} node - HTMLElement
+ * @return {object} - {left, top} : Scroll Position from current viewport, height, width} : Resolution
+ */
 export const getElementPosition = function(node) {
 	let position = node.getBoundingClientRect();
 	return { left: position.left, top: position.top, height: position.height, width: position.width };
 };
 
+/**
+ * Provides Position of current element and resolution
+ * @param {Element} node - HTMLElement
+ * @return {object} - {left, top} : Scroll Position from (0,0), height, width} : Resolution
+ */
 export const getCurrentViewportElementPosition = function(node) {
 	let position = getElementPosition(node);
 	let scrollPos = getCurrentScrollPosition();
