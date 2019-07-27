03a86a3a5cce3668583a119081b13e7d1912f577
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-27 15:57:52 +0530

    test<infra> : added jest rules in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
 	env: {
 		browser: true,
-		es6: true
+		es6: true,
+		'jest/globals': true
 	},
 	parserOptions: {
 		sourceType: 'module',
@@ -9,5 +10,5 @@ module.exports = {
 			experimentalObjectRestSpread: true
 		}
 	},
-	extends: 'eslint:recommended'
+	extends: ['eslint:recommended', 'plugin:jest/recommended']
 };
